8d9b72b6daf2f00044675e5a407d5be76eb9499d
titima-satara <satara_t@silpakorn.edu>
2023-01-13 20:55:28 +0700

    fix

--- src/main/java/com/example/demo/model/ProvincesData.java
+++ src/main/java/com/example/demo/model/ProvincesData.java
@@ -4,10 +4,9 @@ package com.example.demo.model;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
-
-import javax.persistence.Entity;
 
 @Getter
 @Setter
